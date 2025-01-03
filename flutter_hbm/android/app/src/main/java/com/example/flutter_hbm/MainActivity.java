9dad0535448599fe6ee968190cd4b7ae7a2eb05b
v-petrov <vapetrov@tu-sofia.bg>
2025-01-03 11:07:01 +0200

    added screens

---
+++ flutter_hbm/android/app/src/main/java/com/example/flutter_hbm/MainActivity.java
@@ -0,0 +1,6 @@
+package com.example.flutter_hbm;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ flutter_hbm/lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Health Body&Mind',
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+      )
+    );
+  }
+}
