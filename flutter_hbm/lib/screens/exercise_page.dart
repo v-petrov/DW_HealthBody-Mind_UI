23b0aa2e56483a55a7283e7bbff4fe715ae978c9
v-petrov <vapetrov@tu-sofia.bg>
2025-02-10 20:57:52 +0200

    added dropdown menu for the level of training

--- flutter_hbm/lib/screens/exercise_page.dart
+++ flutter_hbm/lib/screens/exercise_page.dart
@@ -4,198 +4,225 @@ import 'package:flutter_hbm/widgets/vertical_scroll.dart';
 import '../widgets/main_layout.dart';
 import '../widgets/calendar.dart';
 
-class ExercisePage extends StatelessWidget {
+class ExercisePage extends StatefulWidget {
   const ExercisePage({super.key});
+
+  @override
+  ExercisePageState createState() => ExercisePageState();
+}
+
+class ExercisePageState extends State<ExercisePage> {
+  String selectedLevel = "";
 
   @override
   Widget build(BuildContext context) {
     return AppLayout(
-        child: LayoutBuilder(
-          builder: (context, constraints) {
-            double minWidth = 1000;
-            double screenWidth = constraints.maxWidth < minWidth
-                ? minWidth
-                : constraints.maxWidth;
-            double screenHeight = constraints.maxHeight;
+      child: LayoutBuilder(
+        builder: (context, constraints) {
+          double minWidth = 1000;
+          double screenWidth = constraints.maxWidth < minWidth
+              ? minWidth
+              : constraints.maxWidth;
+          double screenHeight = constraints.maxHeight;
+          double leftSideWidth = screenWidth * 0.5;
+          double rightSideWidth = screenWidth * 0.5;
 
-            double leftSideWidth = screenWidth * 0.5;
-            double rightSideWidth = screenWidth * 0.5;
-
-            return HorizontalScrollable(
-              child: Row(
-                children: [
-                  Container(
-                    padding: EdgeInsets.all(10),
-                    width: leftSideWidth,
-                    height: screenHeight,
-                    child: VerticalScrollable(
-                      child: Column(
-                        children: [
-                          Row(
-                            children: [
-                              Text(
-                                "Choose a day to see your training: ",
-                                style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-                              ),
-                              SizedBox(width: 10),
-                              Flexible(child: DateSelectionWidget()),
-                            ],
-                          ),
-                          SizedBox(height: 10),
-                          Text("Cardio:", style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
-                          SizedBox(height: 10),
-                          Row(
-                            children: [
-                              SizedBox(
-                                width: 150,
-                                child: Text("How long (minutes):", textAlign: TextAlign.right),
-                              ),
-                              SizedBox(width: 10),
-                              Flexible(child: TextField(decoration: InputDecoration(border: OutlineInputBorder()))),
-                            ],
-                          ),
-                          SizedBox(height: 10),
-                          Row(
-                            children: [
-                              SizedBox(
-                                width: 150,
-                                child: Text("Calories burned:", textAlign: TextAlign.right),
-                              ),
-                              SizedBox(width: 10),
-                              Flexible(child: TextField(decoration: InputDecoration(border: OutlineInputBorder()))),
-                            ],
-                          ),
-                          SizedBox(height: 10),
-                          Row(
-                            children: [
-                              SizedBox(
-                                width: 150,
-                                child: Text("(5435) Steps:\nGoal: 8000", textAlign: TextAlign.right),
-                              ),
-                              SizedBox(width: 10),
-                              Flexible(child: TextField(decoration: InputDecoration(border: OutlineInputBorder(), labelText: "optional"))),
-                            ],
-                          ),
-                          SizedBox(height: 15),
-
-                          Center(
-                            child: ElevatedButton(
-                              onPressed: () {},
-                              child: Text("Add"),
+          return HorizontalScrollable(
+            child: Row(
+              children: [
+                Container(
+                  padding: EdgeInsets.all(10),
+                  width: leftSideWidth,
+                  height: screenHeight,
+                  child: VerticalScrollable(
+                    child: Column(
+                      children: [
+                        Row(
+                          children: [
+                            Text(
+                              "Choose a day to see your training: ",
+                              style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
                             ),
-                          ),
-                          SizedBox(height: 15),
+                            SizedBox(width: 10),
+                            Flexible(child: DateSelectionWidget()),
+                          ],
+                        ),
+                        SizedBox(height: 10),
+                        Text("Cardio:", style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
+                        SizedBox(height: 10),
+                        Row(
+                          children: [
+                            SizedBox(
+                              width: 150,
+                              child: Text("How long (minutes):", textAlign: TextAlign.right),
+                            ),
+                            SizedBox(width: 10),
+                            Flexible(child: TextField(decoration: InputDecoration(border: OutlineInputBorder()))),
+                          ],
+                        ),
+                        SizedBox(height: 10),
+                        Row(
+                          children: [
+                            SizedBox(
+                              width: 150,
+                              child: Text("Calories burned:", textAlign: TextAlign.right),
+                            ),
+                            SizedBox(width: 10),
+                            Flexible(child: TextField(decoration: InputDecoration(border: OutlineInputBorder()))),
+                          ],
+                        ),
+                        SizedBox(height: 10),
+                        Row(
+                          children: [
+                            SizedBox(
+                              width: 150,
+                              child: Text("(5435) Steps:\nGoal: 8000", textAlign: TextAlign.right),
+                            ),
+                            SizedBox(width: 10),
+                            Flexible(child: TextField(decoration: InputDecoration(border: OutlineInputBorder(), labelText: "optional"))),
+                          ],
+                        ),
+                        SizedBox(height: 15),
 
-                          Divider(thickness: 1, color: Colors.black),
-                          SizedBox(height: 10),
-                          Text("Daily result:", style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
-                          SizedBox(height: 10),
-                          Row(
-                            children: [
-                              SizedBox(
-                                width: 150,
-                                child: Text("Duration:", textAlign: TextAlign.right),
-                              ),
-                              SizedBox(width: 10),
-                              Flexible(child: TextField(decoration: InputDecoration(border: OutlineInputBorder()))),
-                            ],
+                        Center(
+                          child: ElevatedButton(
+                            onPressed: () {},
+                            child: Text("Add"),
                           ),
-                          SizedBox(height: 10),
-                          Row(
-                            children: [
-                              SizedBox(
-                                width: 150,
-                                child: Text("Calories burned:", textAlign: TextAlign.right),
-                              ),
-                              SizedBox(width: 10),
-                              Flexible(child: TextField(decoration: InputDecoration(border: OutlineInputBorder()))),
-                            ],
-                          ),
-                        ],
-                      ),
+                        ),
+                        SizedBox(height: 15),
+
+                        Divider(thickness: 1, color: Colors.black),
+                        SizedBox(height: 10),
+                        Text("Daily result:", style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
+                        SizedBox(height: 10),
+                        Row(
+                          children: [
+                            SizedBox(
+                              width: 150,
+                              child: Text("Duration:", textAlign: TextAlign.right),
+                            ),
+                            SizedBox(width: 10),
+                            Flexible(child: TextField(decoration: InputDecoration(border: OutlineInputBorder()))),
+                          ],
+                        ),
+                        SizedBox(height: 10),
+                        Row(
+                          children: [
+                            SizedBox(
+                              width: 150,
+                              child: Text("Calories burned:", textAlign: TextAlign.right),
+                            ),
+                            SizedBox(width: 10),
+                            Flexible(child: TextField(decoration: InputDecoration(border: OutlineInputBorder()))),
+                          ],
+                        ),
+                      ],
                     ),
                   ),
-                  Container(
-                    padding: EdgeInsets.all(10),
-                    width: rightSideWidth,
-                    height: screenHeight,
-                    child: VerticalScrollable(
-                      child: Column(
-                        children: [
-                          SizedBox(height: 67),
-                          Text("Lifting:", style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
-                          SizedBox(height: 10),
-                          Container(
-                            padding: EdgeInsets.all(10),
-                            decoration: BoxDecoration(
-                              border: Border.all(color: Colors.black, width: 1),
-                              borderRadius: BorderRadius.circular(10),
+                ),
+                Container(
+                  padding: EdgeInsets.all(10),
+                  width: rightSideWidth,
+                  height: screenHeight,
+                  child: VerticalScrollable(
+                    child: Column(
+                      children: [
+                        SizedBox(height: 67),
+                        Text("Lifting:", style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
+                        SizedBox(height: 10),
+                        Container(
+                          padding: EdgeInsets.all(10),
+                          decoration: BoxDecoration(
+                            border: Border.all(color: Colors.black, width: 1),
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          child: Text(
+                            "To calculate how many calories a person has expended in weight training, "
+                                "we can use a formula that includes several variables such as weight, "
+                                "time of the workout, intensity and the metabolic equivalent (MET) of the exercise. "
+                                "Here is the basic formula:\n\n"
+                                "Calories = MET × Weight (kg) × Time (hours).\n\n"
+                                "MET (Metabolic Equivalent of Task) is a value that measures the intensity of physical activity.",
+                            style: TextStyle(fontSize: 14),
+                            textAlign: TextAlign.justify,
+                          ),
+                        ),
+                        SizedBox(height: 10),
+                        Row(
+                          children: [
+                            SizedBox(
+                              width: 250,
+                              child: Text("Choose your level of workout activity:", textAlign: TextAlign.right),
                             ),
-                            child: Text(
-                              "To calculate how many calories a person has expended in weight training, "
-                                  "we can use a formula that includes several variables such as weight, "
-                                  "time of the workout, intensity and the metabolic equivalent (MET) of the exercise. "
-                                  "Here is the basic formula:\n\n"
-                                  "Calories = MET × Weight (kg) × Time (hours).\n\n"
-                                  "MET (Metabolic Equivalent of Task) is a value that measures the intensity of physical activity.",
-                              style: TextStyle(fontSize: 14),
-                              textAlign: TextAlign.justify,
+                            SizedBox(width: 10),
+                            Flexible(
+                              child: DropdownButtonFormField<String>(
+                                hint: Text("Select level"),
+                                isExpanded: true,
+                                value: selectedLevel.isEmpty ? null : selectedLevel,
+                                items: [
+                                  "Light intensity (e.g., stretching, warm-ups)",
+                                  "Moderate intensity (e.g., general weightlifting)",
+                                  "High intensity (e.g., vigorous lifting, powerlifting, circuit training)"]
+                                    .map((level) =>
+                                    DropdownMenuItem(
+                                      value: level,
+                                      child: Tooltip(
+                                        message: level,
+                                        child: Text(
+                                          level,
+                                          maxLines: 1,
+                                          overflow: TextOverflow.ellipsis,
+                                        ),
+                                      ),
+                                    ))
+                                    .toList(),
+                                onChanged: (String? newValue) {
+                                  setState(() {
+                                    selectedLevel = newValue!;
+                                  });
+                                  },
+                              ),
                             ),
-                          ),
-                          SizedBox(height: 10),
-                          Row(
-                            children: [
-                              SizedBox(
-                                width: 250,
-                                child: Text("Choose your level of workout activity:", textAlign: TextAlign.right),
-                              ),
-                              SizedBox(width: 10),
-                              Flexible(
-                                child: TextField(
-                                  decoration: InputDecoration(
-                                    border: OutlineInputBorder(),
-                                    labelText: "Select level",
-                                  ),
-                                ),
-                              ),
-                            ],
-                          ),
-                          SizedBox(height: 10),
-                          Row(
-                            children: [
-                              SizedBox(
-                                width: 250,
-                                child: Text("Time (hours):", textAlign: TextAlign.right),
-                              ),
-                              SizedBox(width: 10),
-                              Flexible(child: TextField(decoration: InputDecoration(border: OutlineInputBorder())),
-                              ),
-                            ],
-                          ),
-                          SizedBox(height: 15),
-                          Center(
-                            child: ElevatedButton(
-                              onPressed: () {},
-                              child: Text("Calculate"),
+                          ],
+                        ),
+                        SizedBox(height: 10),
+                        Row(
+                          children: [
+                            SizedBox(
+                              width: 250,
+                              child: Text("Time (hours):", textAlign: TextAlign.right),
                             ),
+                            SizedBox(width: 10),
+                            Flexible(child: TextField(decoration: InputDecoration(border: OutlineInputBorder())),
+                            ),
+                          ],
+                        ),
+                        SizedBox(height: 15),
+                        Center(
+                          child: ElevatedButton(
+                            onPressed: () {},
+                            child: Text("Calculate"),
                           ),
-                          SizedBox(height: 15),
-                          Row(
-                            children: [
-                              Text("Calories burned: "),
-                              SizedBox(width: 10),
-                              Flexible(child: TextField(decoration: InputDecoration(border: OutlineInputBorder()))),
-                            ],
-                          ),
-                        ],
-                      ),
+                        ),
+                        SizedBox(height: 15),
+                        Row(
+                          children: [
+                            Text("Calories burned: "),
+                            SizedBox(width: 10),
+                            Flexible(child: TextField(decoration: InputDecoration(border: OutlineInputBorder()))),
+                          ],
+                        ),
+                      ],
                     ),
                   ),
-                ],
-              ),
-            );
-          }
-        )
-      );
-    }
+                ),
+              ],
+            ),
+          );
+        }
+      )
+    );
   }
+}
