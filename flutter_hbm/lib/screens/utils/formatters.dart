db7b51817f439206aa07aa76969a3d21b7a77997
v-petrov <vapetrov@tu-sofia.bg>
2025-02-23 18:35:54 +0200

    added a water formatter and changed the integer formatter

--- flutter_hbm/lib/screens/utils/formatters.dart
+++ flutter_hbm/lib/screens/utils/formatters.dart
@@ -1,5 +1,24 @@
 import 'package:flutter/services.dart';
 
+class WaterTextInputFormatter extends TextInputFormatter {
+  final int decimalPlaces;
+
+  WaterTextInputFormatter({this.decimalPlaces = 1});
+
+  @override
+  TextEditingValue formatEditUpdate(TextEditingValue oldValue, TextEditingValue newValue) {
+    final text = newValue.text;
+
+    final regExp = RegExp('^\\d{0,2}(\\.\\d{0,$decimalPlaces})?\$');
+
+    if (regExp.hasMatch(text)) {
+      return newValue;
+    } else {
+      return oldValue;
+    }
+  }
+}
+
 class DecimalTextInputFormatter extends TextInputFormatter {
   final int decimalPlaces;
 
@@ -19,12 +38,14 @@ class DecimalTextInputFormatter extends TextInputFormatter {
   }
 }
 
-class HeightTextInputFormatter extends TextInputFormatter {
+class IntegerTextInputFormatter extends TextInputFormatter {
+  int? num;
+  IntegerTextInputFormatter(int this.num);
   @override
   TextEditingValue formatEditUpdate(TextEditingValue oldValue, TextEditingValue newValue) {
     final text = newValue.text;
 
-    final regExp = RegExp(r'^\d{0,3}$');
+    final regExp = RegExp(r'^\d{0,' + num.toString() + r'}$');
 
     if (regExp.hasMatch(text)) {
       return newValue;
